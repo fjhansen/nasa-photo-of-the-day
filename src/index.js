e4722b925b3dfbfb03cbcaaaa42d931de0656e0d
Frances Hansen <francesjuniperhansen@gmail.com>
2020-02-20 13:30:21 -0800

    updated styling before refractor

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./App.css";
 function App() {
     
     const [planets, setPlanets] = useState([]);
+    let url = "https://lh3.googleusercontent.com/proxy/7jGGdiWpMIUOBm7XHp78bQO3EQ_t1FqHVTOYqvEiJJJ7CrKvG6avj5Dm8KO2tiABaq_j4GdFWYx1pXDd15e7e1kkYb3yhfPu8gDU-zpY1PKqydwzn4JIYlBEbwWg70HYrR6FE7eNXK4WsxiUuujhvTCxY-DE-mL8nUVA7GUtUEQNY6GZ"
 
 const fetchSpace = () => {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=qGXu0u8klvk6zWxUEdFhAsGmagoOWGMMZZQYjk1O`)
@@ -29,10 +30,10 @@ useEffect(() => {
 return (
     
         <div id="top" className="App-header">
-        <header id="top" className="App-logo-spin">
-        <div>
-        <div className="App-logo">🚀</div>
-        <div className="App-logo">🚀</div>
+        <header id="top" >
+        <div className="App-logo-spin">
+        <div className="App-logo"></div>
+        <div className="App-logo"></div>
         <div className="App-logo">🚀</div>    
                 
         </div>
@@ -41,20 +42,21 @@ return (
         <div className="liftUp">
         
         
-           
+           <div class="imgs">
             <img className="stubborn" width="68%" src={planets.url}/>
-            
+            <img className="nasa" src={url}/>
+            </div>
         
         <p className="title">{planets.title}</p>
         <div id="bottom">{planets.explanation}</div>   
         </div>  
 
-        <header  className="App-logo-spin">
-          
+        <header>
+          <div className="App-logo-spin">
+        <div className="App-logo"></div>
         <div className="App-logo">🚀</div>
-        <div className="App-logo">🚀</div>
-        <div className="App-logo">🚀</div>
-             
+        <div className="App-logo"></div>
+            </div> 
         
         </header>
         </div>
